27a596fa770366003ab139f788d1ffacd1cd89cc
gsulay <sulaygifrey@gmail.com>
2025-02-22 16:48:41 +0800

    logger now creates a log file which can be viewed through the cli

--- module/logger.py
+++ module/logger.py
@@ -1,20 +1,28 @@
 import logging
 
 class Logger:
-    def __init__(self, name=__name__):
+    def __init__(self, name=__name__, log_file="app.log"):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.INFO)
 
-        # Create a console handler and set level to info
+        # Create a console handler
         ch = logging.StreamHandler()
         ch.setLevel(logging.INFO)
 
-        # Create a formatter and set it for the handler
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
+        # Create a file handler
+        fh = logging.FileHandler(log_file)
+        fh.setLevel(logging.INFO)
 
-        # Add the handler to the logger
+        # Create a formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # Set formatter for handlers
+        ch.setFormatter(formatter)
+        fh.setFormatter(formatter)
+
+        # Add handlers to the logger
         self.logger.addHandler(ch)
+        self.logger.addHandler(fh)
 
     def log(self, level, message):
         if level == 'info':
